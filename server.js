d6c0f928d5d67f0e91d7b7366172c9a7922af033
Liam <20miller13@gmail.com>
2024-04-23 17:25:26 +1000

    Add scraper and connect to app

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-const express = require('express');
-
+const express = require("express");
+const axios = require("axios");
+const scraper_api_endpoint = "http://127.0.0.1:5000/";
 
 const app = express();
 const PORT = 4000;
 
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", async (req, res) => {
+  res.send("<h1>Hello World!</h1><a href='/movies'>See popular movies</a>");
 });
-
+app.get("/movies", async (req, res) => {
+  try {
+    const response = await axios.get(scraper_api_endpoint);
+    res.send(response.data);
+  } catch (error) {
+    console.error("Failed to fetch data: ", error);
+    res.status(500).send("Failed to fetch data");
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
-
-
-
