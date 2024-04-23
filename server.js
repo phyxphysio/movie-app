cf87cda1eccf231c84a7e21270d931588a9c7cb7
Liam <20miller13@gmail.com>
2024-04-23 18:39:02 +1000

    Update index.html to show movies

--- server.js
+++ server.js
@@ -4,14 +4,24 @@ const scraper_api_endpoint = "http://127.0.0.1:5000/";
 
 const app = express();
 const PORT = 4000;
+app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  res.send("<h1>Hello World!</h1><a href='/movies'>See popular movies</a>");
+  //   res.send("<h1>Hello World!</h1><a href='/movies'>See popular movies</a>");
+  res.sendFile("index.html");
 });
 app.get("/movies", async (req, res) => {
   try {
     const response = await axios.get(scraper_api_endpoint);
-    res.send(response.data);
+    let htmlResponse = "<!DOCTYPE html><html lang='en'><head><title>User Info</title></head><body>";
+    htmlResponse += "<h1>Popular Movies</h1>";
+
+    for (const key in response.data) {
+        htmlResponse += `<p>${key}: ${JSON.stringify(response.data[key])}</p>`;
+    }
+
+    htmlResponse += "</body></html>";
+    res.send(htmlResponse);
   } catch (error) {
     console.error("Failed to fetch data: ", error);
     res.status(500).send("Failed to fetch data");
