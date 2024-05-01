0e01103042e57e80c1128ff4cb1490b55872896b
MichaelK <130948554+MichaelK2023@users.noreply.github.com>
2024-05-01 11:26:46 +1000

    post route for registering added

--- server.js
+++ server.js
@@ -5,22 +5,27 @@ const app = express();
 const PORT = 4000;
 
 
-
-async function createUser(username, password) {
-    try {
-        const newUser = await User.create({ username, password});
-        console.log('User created:', newUser);
-    } catch (err) {
-        console.error('Error creating user:', err);
-    }
-}
-
-createUser('test', 'test123');
+// middleware to parse JSON bodies
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.post('/register', async (req, res) => {
+    try {
+        const { username, password} = req.body;
+
+        const newUser = await User.create({ username, password });
+
+        res.status(201).json({ message: 'User registered successfully', user: newUser });
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).json({ message: 'An error occurred while registering user' });
+    }
+});
+
 
 
 app.listen(PORT, () => {
